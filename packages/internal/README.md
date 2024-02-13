c54bdfc24db9b7c660eadd043fe727c5e5ed5b39
E0SelmY4V <yabo1887415157@qq.com>
2024-02-14 04:57:57 +0800

    Add(internal): 迁移internal包
    把内部类型包也迁移过来啦…

---
+++ packages/internal/README.md
@@ -0,0 +1,10 @@
+# 精确类型～内部工具包
+
+[![Type Lint](https://github.com/accurtype/accurtype/actions/workflows/lint.yaml/badge.svg)](https://github.com/accurtype/accurtype/actions/workflows/lint.yaml)
+
+本包包含 _Accurtype_ 中的内部工具。
+
+**请不要直接将本包用于你的项目！**
+除非你在为 _Accurtype_ 开发其他包。
+
+详情请参阅代码注释。
---
+++ packages/internal/lib/calculate/step.ts
@@ -0,0 +1,57 @@
+/**
+ * 步进器
+ * @version 1.0.1
+ * @license MIT
+ */
+declare module './step';
+const Test = true; Test;
+
+import { Digit, NumberOf } from '@accurtype/base';
+import { ReversedString, Zeroless } from '../common';
+import { Direction } from './base';
+
+type SigVary<D, N extends Digit> = (D extends 0
+	? [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+	: [9, 0, 1, 2, 3, 4, 5, 6, 7, 8]
+)[N];
+type SteppedString<
+	D extends Direction,
+	N extends string,
+	B extends string = '',
+	E extends Direction = Exclude<Direction, D>>
+	= (N extends `${E}${infer N}`
+		? SteppedString<D, N, `${B}${D}`>
+		: (N extends `${infer S extends Digit}${infer N}`
+			? `${B}${SigVary<D, S>}${N}`
+			: `${B}${N}`
+		)
+	);
+export type Stepped<D extends Direction, N extends number> = NumberOf<ReversedString<Zeroless<SteppedString<D, ReversedString<`0${N}`>>>>>;
+
+export type Decreased<N extends number> = Stepped<9, N>;
+namespace Test {
+	// $ExpectType 0
+	export type EQ = Decreased<1>;
+	// $ExpectType 4
+	export type SZ = Decreased<5>;
+	// $ExpectType 9
+	export type GT = Decreased<10>;
+	// $ExpectType 9999
+	export type HU = Decreased<10000>;
+	// $ExpectType 190379909999
+	export type SD = Decreased<190379910000>;
+}
+
+export type Increased<N extends number> = Stepped<0, N>;
+namespace Test {
+	// $ExpectType 1
+	export type AK = Increased<0>;
+	// $ExpectType 2
+	export type AS = Increased<1>;
+	// $ExpectType 10000
+	export type EZ = Increased<9999>;
+	// $ExpectType 1023200
+	export type FR = Increased<1023199>;
+	// $ExpectType 918936199000
+	export type HG = Increased<918936198999>;
+}
